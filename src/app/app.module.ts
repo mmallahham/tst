63990c7cc229098e68103dd65e51463af83eeffa
mmallahham <mmallahham@hotmail.com>
2018-01-18 07:19:13 +0100

    add router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule , Component } from '@angular/core';
+import { RouterModule } from '@angular/router'
+import {APP_BASE_HREF} from '@angular/common';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path:'home',component: HomeComponent},
+      {path:'',component: HomeComponent},
+      {path:'**',component: HomeComponent}
+    ])
   ],
-  providers: [],
+  providers: [{provide: APP_BASE_HREF, useValue: '/'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
